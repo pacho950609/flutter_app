b01cdcb903c3de42ba950b309552be8321729b47
Francisco Ricaurte <francisco@tyba.com.co>
2020-05-23 09:41:33 -0500

    Init commit

---
+++ lib/desription_place.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+
+enum StarType { COMPLETE, EMPTY, HALF }
+
+class DescriptionPlace extends StatelessWidget {
+  final String name;
+  final int starsNumber;
+  final String description;
+
+  DescriptionPlace(this.name, this.description, this.starsNumber);
+
+  Widget starWidget(StarType type) {
+    IconData icon;
+    switch (type) {
+      case StarType.COMPLETE:
+        icon = Icons.star;
+        break;
+      case StarType.EMPTY:
+        icon = Icons.star_border;
+        break;
+      case StarType.HALF:
+        icon = Icons.star_half;
+        break;
+      default:
+    }
+
+    final star = Container(
+      margin: EdgeInsets.only(top: 323, left: 3),
+      child: Icon(
+        icon,
+        color: Colors.yellow,
+      ),
+    );
+    return star;
+  }
+
+  Widget build(BuildContext context) {
+    final starWidgetArray = [];
+    for (var i = 0; i < 5; i++) {
+      if (starsNumber >= (i + 1)) {
+        starWidgetArray.add(starWidget(StarType.COMPLETE));
+      } else {
+        starWidgetArray.add(starWidget(StarType.EMPTY));
+      }
+    }
+
+    final titleStarsWidget = Row(
+      children: <Widget>[
+        Container(
+          margin: EdgeInsets.only(top: 320, left: 20, right: 20),
+          child: Text(
+            name,
+            style: TextStyle(fontSize: 30, fontWeight: FontWeight.w900),
+          ),
+        ),
+        ...starWidgetArray
+      ],
+    );
+
+    final descriptionWidget = Container(
+      child: Text(
+        description,
+        style: TextStyle(fontSize: 15,
+        fontFamily: ''),
+      ),
+      margin: EdgeInsets.only(top: 5, left: 20, right: 20),
+    );
+
+    final descriptionPlace = Column(
+      children: <Widget>[titleStarsWidget, descriptionWidget],
+    );
+
+    return descriptionPlace;
+  }
+}
---
+++ lib/review.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+class Review extends StatelessWidget {
+  String name;
+  int reviewsNumber;
+  int photosNumber;
+  String details;
+  String urlPhoto;
+  int starNumber;
+  // Review(title, reviewsNumber, photosNumber, description, urlPhoto, starNumber);
+  Review(this.name, this.reviewsNumber, this.photosNumber, this.details);
+
+  @override
+  Widget build(BuildContext context) {
+    final userComments = Container(
+      margin: EdgeInsets.only(left: 20, top: 3),
+      child: Text(
+        details,
+        style: TextStyle(fontSize: 14, fontWeight: FontWeight.w900),
+        overflow: TextOverflow.ellipsis,
+      ),
+    );
+
+    final userInfo = Container(
+      margin: EdgeInsets.only(left: 20, top: 3),
+      child: Text(
+        '${reviewsNumber} reviews - ${photosNumber} photos',
+        style: TextStyle(fontSize: 14, color: Colors.grey),
+      ),
+    );
+
+    final userName = Container(
+      margin: EdgeInsets.only(left: 20, top: 10),
+      child: Text(
+        name,
+        style: TextStyle(fontSize: 18),
+      ),
+    );
+
+    final userDetails = Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[userName, userInfo,userComments],
+    );
+
+    final image = Container(
+      margin: EdgeInsets.only(left: 20, top: 20),
+      width: 80,
+      height: 80,
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        image: DecorationImage(
+            image: NetworkImage('https://via.placeholder.com/140x100'),
+            fit: BoxFit.cover),
+      ),
+    );
+
+    final reviewContainer = Container(
+      child: Row(
+        children: <Widget>[image, userDetails],
+      ),
+    );
+
+    return reviewContainer;
+  }
+}
